e5fd0158f9e7c410cbc9c79b19144f056be0f910
Alec Ananian <>
2020-06-08 21:04:00 -0700

    Added loading display to login dialog

--- src/components/LoginDialog/LoginDialog.tsx
+++ src/components/LoginDialog/LoginDialog.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import {
   Box,
+  Button,
+  CircularProgress,
   Dialog,
   DialogContent,
   Typography,
@@ -10,19 +12,15 @@ import {
 
 interface Props {
   open: boolean;
+  loading: boolean;
   onLogIn: () => void;
 }
 
-const LoginButton = styled.button`
-  width: 193px;
-  height: 48px;
-  background: url(/images/btn_strava_connectwith_orange@2x.png) no-repeat;
-  background-size: 100%;
-  border: 0;
-  cursor: pointer;
-`;
-
-const LoginDialog = ({ open = true, onLogIn }: Props) => {
+const LoginDialog = ({
+  open = false,
+  loading = false,
+  onLogIn,
+}: Props) => {
   const { t } = useTranslation();
   return (
     <Dialog
@@ -33,19 +31,31 @@ const LoginDialog = ({ open = true, onLogIn }: Props) => {
       aria-describedby="login-dialog-description"
     >
       <DialogContent>
-        <Typography variant="h4" align="center">
-          {t('app.title')}
-        </Typography>
-        <Typography
-          id="login-dialog-description"
-          component="div"
-          align="center"
-        >
-          {t('app.description')}
-        </Typography>
-        <Box my={2} textAlign="center">
-          <LoginButton onClick={onLogIn} />
-        </Box>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <Typography variant="h4" align="center">
+              {t('app.title')}
+            </Typography>
+            <Typography
+              id="login-dialog-description"
+              component="div"
+              align="center"
+            >
+              {t('app.description')}
+            </Typography>
+            <Box my={2} textAlign="center">
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={onLogIn}
+              >
+                Connect with Strava
+              </Button>
+            </Box>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
